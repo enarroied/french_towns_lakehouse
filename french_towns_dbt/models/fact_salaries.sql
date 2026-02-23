6c25bf6566cf149a1872dba99221f040d680250f
enarroied <eric.narro.ied@gmail.com>
2026-02-23 20:24:41 +0100

    added colums to fact salaries

--- french_towns_dbt/models/fact_salaries.sql
+++ french_towns_dbt/models/fact_salaries.sql
@@ -6,12 +6,24 @@
 SELECT
     GEO::CHAR(5)                                          AS id,
     TIME_PERIOD::INTEGER                                  AS year,
-    MAX(CASE WHEN SEX = 'M'  THEN OBS_VALUE::INTEGER END) AS mean_salary_men,
-    MAX(CASE WHEN SEX = 'F'  THEN OBS_VALUE::INTEGER END) AS mean_salary_women,
-    MAX(CASE WHEN SEX = '_T' THEN OBS_VALUE::INTEGER END) AS mean_salary
+    MAX(CASE WHEN SEX = 'M' AND PCS_ESE = '_T' THEN OBS_VALUE::INTEGER END) AS mean_salary_men,
+    MAX(CASE WHEN SEX = 'F' AND PCS_ESE = '_T' THEN OBS_VALUE::INTEGER END) AS mean_salary_women,
+    MAX(CASE WHEN SEX = '_T' AND PCS_ESE = '1T3' THEN OBS_VALUE::INTEGER END) AS mean_salary_management_position,
+    MAX(CASE WHEN SEX = '_T' AND PCS_ESE = '4' THEN OBS_VALUE::INTEGER END) AS mean_salary_intermediate_position,
+    MAX(CASE WHEN SEX = '_T' AND PCS_ESE = '5' THEN OBS_VALUE::INTEGER END) AS mean_salary_service_workers,
+    MAX(CASE WHEN SEX = '_T' AND PCS_ESE = '6' THEN OBS_VALUE::INTEGER END) AS mean_salary_manual_workers,
+    MAX(CASE WHEN SEX = 'M' AND PCS_ESE = '1T3' THEN OBS_VALUE::INTEGER END) AS mean_salary_management_position_men,
+    MAX(CASE WHEN SEX = 'M' AND PCS_ESE = '4' THEN OBS_VALUE::INTEGER END) AS mean_salary_intermediate_position_men,
+    MAX(CASE WHEN SEX = 'M' AND PCS_ESE = '5' THEN OBS_VALUE::INTEGER END) AS mean_salary_service_workers_men,
+    MAX(CASE WHEN SEX = 'M' AND PCS_ESE = '6' THEN OBS_VALUE::INTEGER END) AS mean_salary_manual_workers_men,
+    MAX(CASE WHEN SEX = 'F' AND PCS_ESE = '1T3' THEN OBS_VALUE::INTEGER END) AS mean_salary_management_position_women,
+    MAX(CASE WHEN SEX = 'F' AND PCS_ESE = '4' THEN OBS_VALUE::INTEGER END) AS mean_salary_intermediate_position_women,
+    MAX(CASE WHEN SEX = 'F' AND PCS_ESE = '5' THEN OBS_VALUE::INTEGER END) AS mean_salary_service_workers_women,
+    MAX(CASE WHEN SEX = 'F' AND PCS_ESE = '6' THEN OBS_VALUE::INTEGER END) AS mean_salary_manual_workers_women,
+    MAX(CASE WHEN SEX = 'F' AND PCS_ESE = '_T' THEN OBS_VALUE::INTEGER END) AS mean_salary
 FROM {{ source('french_towns', 'salaries') }}
 WHERE
     GEO_OBJECT = 'COM'
-    AND PCS_ESE = '_T'
+
 GROUP BY GEO, TIME_PERIOD
 ORDER BY GEO, year
