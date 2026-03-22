289781f9d6d94e7c7f609a800428be82bc6e60b4
enarroied <enarroied@users.noreply.github.com>
2026-03-22 21:35:32 +0000

    deploy: 73e304520df2c4637d49ed4c4cea3263afefbf21

--- docs/compiled/french_towns_dbt/models/fact_population.sql
+++ docs/compiled/french_towns_dbt/models/fact_population.sql
@@ -1,8 +1,25 @@
 
 
+WITH base AS (
+    SELECT
+        GEO::CHAR(5)            AS id,
+        TIME_PERIOD::INTEGER    AS year,
+        OBS_VALUE::INTEGER      AS population
+    FROM read_csv_auto('../input/DS_POPULATIONS_HISTORIQUES_data.csv', sample_size=-1)
+    WHERE GEO_OBJECT = 'COM'
+)
 SELECT
-    GEO::CHAR(5)            AS id,
-    TIME_PERIOD::INTEGER    AS year,
-    OBS_VALUE::INTEGER      AS population
-FROM read_csv_auto('../input/DS_POPULATIONS_HISTORIQUES_data.csv', sample_size=-1)
-WHERE GEO_OBJECT = 'COM'
+    b.id,
+    b.year,
+    b.population,
+    (b.population - p.population) AS year_evolution,
+    CASE
+        WHEN p.population IS NOT NULL AND p.population != 0
+        THEN ((b.population - p.population) * 100.0 / p.population)
+        ELSE NULL
+    END AS year_evolution_percent
+FROM base b
+LEFT JOIN base p
+    ON b.id = p.id
+    AND b.year = p.year + 1
+ORDER BY b.id, b.year
