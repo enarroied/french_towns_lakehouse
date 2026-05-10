53763d1f7c1572ff86fea1fa7d096b5e412fbb1d
enarroied <eric.narro.ied@gmail.com>
2026-05-10 13:02:23 +0200

    one more time

--- french_towns_dbt/macros/latest_file.sql
+++ french_towns_dbt/macros/latest_file.sql
@@ -1,17 +1,22 @@
 {% macro latest_file(pattern) %}
-    {% set query %}
-        SELECT max(file) AS file
-        FROM glob('{{ pattern }}')
-    {% endset %}
-
-    {% set result = run_query(query) %}
-    {% if execute %}
-        {% set file = result.columns[0].values()[0] %}
-        {% if file is none %}
-            {% do exceptions.raise_compiler_error("No matching GeoJSON files found: " ~ pattern) %}
-        {% endif %}
-        '{{ file }}'
+    {# In CI (docs only), skip the S3 glob lookup and return the pattern as-is #}
+    {% if env_var('CI', 'false') == 'true' %}
+        '{{ pattern }}'
     {% else %}
-        NULL
+        {% set query %}
+            SELECT max(file) AS file
+            FROM glob('{{ pattern }}')
+        {% endset %}
+
+        {% set result = run_query(query) %}
+        {% if execute %}
+            {% set file = result.columns[0].values()[0] %}
+            {% if file is none %}
+                {% do exceptions.raise_compiler_error("No matching GeoJSON files found: " ~ pattern) %}
+            {% endif %}
+            '{{ file }}'
+        {% else %}
+            NULL
+        {% endif %}
     {% endif %}
 {% endmacro %}
